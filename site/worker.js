b910fbc474d896eda137c56be567c5faf6373fd8
Yasuhiro Kudo <ykudo@ykudo.org>
2026-07-12 04:47:18 +0900

    chore: remove playground deployment

--- site/worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    if (url.pathname === '/' || url.pathname === '') {
-      return Response.redirect(`${url.origin}/cbor/`, 301);
-    }
-    return env.ASSETS.fetch(request);
-  },
-};
